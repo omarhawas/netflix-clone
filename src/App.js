0aabe8408a26ca87912466646b99a0096bf76d9e
Omar Hawas <omarhawas94@gmail.com>
2022-09-15 16:54:57 -0400

    poster now showing in UI. Added css to Row.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      {/* NavBar */}
+      {/* <Banner /> */}
       <h1>Netflix Clone</h1>
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/Banner.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import requests from "./requests";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <header>
+      {" "}
+      {/* Background image*/}
+      {/* title */}
+      {/* div and 2 buttons */}
+      {/* description */}
+    </header>
+  );
+}
+
+export default Banner;
